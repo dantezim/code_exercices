a11b9e504f3e4202e5d1df5de305cf9750489d39
Pedro Armada <dante123imp@gmail.com>
2024-01-08 15:29:16 -0300

    Adicionado mais explicações nos códigos

--- treino_array.js
+++ treino_array.js
@@ -1,12 +1,27 @@
+// Soma de um array.
+/* 
+let numerosArr = [10, 20, 30, 20]; // Array de inteiros
+let contador = 0; // Contador para loopar pelo array
+let soma = 0; // variável que irá guardar o resultado da soma do Array
 
-let numerosArr = [10, 20, 30, 20];
-let contador = 0;
-let soma = 0;
-
-for (contador; contador < numerosArr.length; contador++)
+for (contador; contador < numerosArr.length; contador++) // Loop do Array: a variável 'contador' é o início, e o seu limite é contador menor que o tamanho do array. Adiciona 1 no contador a cada volta realizada.
 {
-    soma = soma + numerosArr[contador];
+    soma = soma + numerosArr[contador]; // 'Contador' foi inserido no índice do array numerosArr para representar qual índice será acessado.
 }
 
 console.log("A soma dos elementos é:" + " " + soma);
+
+ */
+
+// Loopando no array de uma forma mais simples (for...of é uma nova funcionalidade do JavaScript)
+/* 
+let numsArr = [10, 20, 30];
+let sumArr = 0;
+
+for(let nums of numsArr)
+{
+    console.log(nums);
+}
+
+ */
 
