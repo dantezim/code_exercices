936852c665144d1718bc941fe7efdceca280579d
Pedro Armada <dante123imp@gmail.com>
2024-01-08 15:14:22 -0300

    Foi adicionado uma parte dedicada a particar arrays.

---
+++ treino_array.js
@@ -0,0 +1,12 @@
+
+let numerosArr = [10, 20, 30, 20];
+let contador = 0;
+let soma = 0;
+
+for (contador; contador < numerosArr.length; contador++)
+{
+    soma = soma + numerosArr[contador];
+}
+
+console.log("A soma dos elementos é:" + " " + soma);
+
